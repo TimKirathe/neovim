cd6a44ef99197679440bf3db0150d7fc56ba91d5
Timothy Kirathe <timothy.kirathe@student.manchester.ac.uk>
2025-06-30 02:38:21 +0300

    alpha: replace avante with claude-code.

--- lua/timothy/plugins/alpha.lua
+++ lua/timothy/plugins/alpha.lua
@@ -24,7 +24,7 @@ return {
 			dashboard.button("SPC ff", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
 			dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
 			dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
-			dashboard.button("SPC aa", "  > Trigger avante", "<cmd>AvanteAsk position=right<CR>"),
+			dashboard.button("SPC cc", "  > Toggle Claude Code", "<cmd>ClaudeCode<CR>"),
 			dashboard.button("q", "  > Quit NVIM", "<cmd>qa<CR>"),
 		}
 
